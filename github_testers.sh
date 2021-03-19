2ca20666b1b885702eeb8ad9c51061b0e9a54f9b
Luis Geniole <luis.geniole@gmail.com>
2021-03-19 05:01:31 -0300

    Update testers script.

--- github_testers.sh
+++ github_testers.sh
@@ -1,36 +1,37 @@
 # INSTALL VALGRIND
 cd ~
-rm -rf $HOME/.brew && \
-	git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew && \
-	echo 'export PATH=$HOME/.brew/bin:$PATH' >> $HOME/.zshrc && \
-	source $HOME/.zshrc && \
+rm -rf $HOME/.brew &&
+	git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew &&
+	echo 'export PATH=$HOME/.brew/bin:$PATH' >>$HOME/.zshrc &&
+	source $HOME/.zshrc &&
 	brew update
-curl https://raw.githubusercontent.com/sowson/valgrind/master/valgrind.rb > valgrind.rb
+curl https://raw.githubusercontent.com/sowson/valgrind/master/valgrind.rb >valgrind.rb
 brew install --HEAD valgrind.rb
 
 # CLONE YOUR PROJECT
 cd ~
-git clone 42_INTRA_REPO_ADDRESS gnl
-cd gnl
-norminette *.c *.h
+git clone 42_INTRA_REPO_ADDRESS ft_printf
+cd ft_printf
+norminette
+make norme
 
 # CLONE ALL GITHUB TESTERS
-git clone https://github.com/cclaude42/PFT_2019.git
 git clone https://github.com/Mazoise/42TESTERS-PRINTF.git
-git clone https://github.com/cacharle/ft_printf_test.git
 chmod 777 */*.sh
-
-# RUN PFT_2019
-cd PFT_2019
-
-cd ..
 
 # RUN 42TESTERS-PRINTF
 cd 42TESTERS-PRINTF
-
+bash run_test.sh
 cd ..
 
-# RUN ft_printf_test
-cd ft_printf_test
+# RUN PFT_2019
+git clone https://github.com/gavinfielder/pft.git pft_2019 && echo "pft_2019/" >>.gitignore && cd pft_2019 && rm unit_tests.c && rm options-config.ini && git clone https://github.com/cclaude42/PFT_2019.git temp && cp temp/unit_tests.c . && cp temp/options-config.ini . && rm -rf temp
+make re
+./test
+cd ../..
 
+# RUN ft_printf_test
+git clone git@github.com:cacharle/ft_printf_test.git
+cd ft_printf_test
+make run
 cd ..
