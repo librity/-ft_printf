93dafa738739d0b0d18ecb154e8a36e54e5bab08
Luis Geniole <luis.geniole@gmail.com>
2021-03-19 02:51:38 -0300

    Import libft.

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 03:17:10 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2021/03/19 01:04:04 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2021/03/19 02:46:18 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,7 @@
 # include <stdbool.h>
 # include <limits.h>
 
-// REMOVE THIS BEFORE SUBMITTING
-#ifdef __linux__
-# include <stdio.h>
-# define ARG_MAX 131072
-#endif
-// REMOVE THIS BEFORE SUBMITTING
+# include "./libft/libft.h"
 
 # define DECIMAL_BASE "0123456789"
 # define DOWNCASE_HEX_BASE "0123456789abcdef"
@@ -167,72 +162,5 @@ void			printf_p(t_printf *print_control,
 void			printf_hex(t_printf *print_control,
 							t_handle_hex *control,
 							t_parse_flags *flag_control);
-
-void			ft_putchar(char c);
-void			ft_putstr(char *s);
-void			ft_putstr_up_to(char *s, size_t up_to);
-void			ft_putendl(char *s);
-
-bool			ft_is_valid_base(const char *base, const size_t base_length);
-void			ft_putnbr_base(int number, char *base);
-void			ft_putnbr_base_i(int number, char *base);
-void			ft_putnbr_base_ui(unsigned int number, const char *base);
-void			ft_putnbr_base_ul(unsigned long number, const char *base);
-void			ft_putnbr_base_li(long int number, const char *base);
-
-void			ft_aux_handle_minus_sign_li(long int *number_pointer);
-
-void			ft_putnbr(int n);
-void			ft_putnbr_i(int number);
-void			ft_putnbr_ui(unsigned int number);
-void			ft_putnbr_ul(unsigned long number);
-void			ft_putnbr_li(long int number);
-
-void			ft_puthex_uppercase(unsigned int number);
-void			ft_puthex_downcase(unsigned int number);
-
-char			*ft_itoa(int n);
-unsigned int	ft_i_to_buffer(int n, char *buffer);
-int				ft_atoi(const char *number_pointer);
-unsigned int	ft_atoui(const char *number_pointer);
-unsigned int	ft_atoui_strict(const char *number_pointer);
-
-unsigned int	ft_count_digits(int number);
-unsigned int	ft_count_digits_i(int number);
-unsigned int	ft_count_digits_i(int number);
-unsigned int	ft_count_digits_ui(unsigned int number);
-unsigned int	ft_count_digits_ul(unsigned long number);
-
-unsigned int	ft_count_digits_hex_ui(unsigned int number);
-unsigned int	ft_count_digits_hex_ul(unsigned long number);
-
-unsigned int	ft_count_chars_i(int number);
-
-char			*ft_skip_digits(char *digits);
-char			*ft_skip_number(char *digits);
-
-bool			ft_isdigit(int c);
-bool			ft_is_whitespace(char character);
-bool			ft_is_plus_or_minus(char character);
-bool			ft_is_decimal_char(char character);
-
-size_t			ft_strlen(const char *s);
-size_t			ft_strlcat(char *dst, const char *src, size_t size);
-char			*ft_strcpy(char *dest, char *src);
-void			ft_strdel(char **delete_me);
-char			*ft_substr(char const *s, unsigned int start, size_t len);
-char			*ft_strdup(const char *s);
-char			*ft_strchr(const char *s, int c);
-char			*ft_strnchr(const char *s, int c, unsigned int limit);
-void			*ft_memcpy(void *dest, const void *src, size_t n);
-
-char			*ft_strjoin(char const *s1, char const *s2);
-char			*ft_strjoin_and_free(char *free_me, char const *dont_free_me);
-char			*ft_strjoin_and_free_free(char *free_me, char *free_me_too);
-char			*ft_strjoin_and_del(char *delete_me,
-									char const *dont_delete_me);
-char			*ft_strjoin_and_del_del(char *delete_me, char *delete_me_too);
-
-bool			unless(bool condition);
 
 #endif
